a33ab474ca8d555a1d5ad9d033823c2aad3c7443
Jeff Blagdon <jeffblagdon@gmail.com>
2014-02-07 17:02:47 -0500

    busbud.py was loading wrong template

--- busbud.py
+++ busbud.py
@@ -7,10 +7,12 @@ D = decimal.Decimal
 PREC = D('0.01')
 
 
-class DataStore(object):
+class CountryDataStore(object):
     def __init__(self, colnames, table_url):
         raw = pd.read_table(table_url, names=colnames)
         frame = raw[raw['country code'].notnull()]
+
+        # Could be abstract, but baking it in doesn't hurt the example
         frame = frame[['geonameid', 'name', 'asciiname', 'latitude',
                        'longitude', 'country code',
                        'population']].set_index('geonameid')
@@ -19,39 +21,42 @@ class DataStore(object):
         for item in target:
             if item in frame.index:
                 frame['in target'].ix[item] = True
-        self.frame = frame.set_index(['country code']).sort_index()
+        self.frame = frame
+        self.country_frame = frame.set_index(['country code']).sort_index()
 
     def get_counts(self, key=None, column=None):
-        return_dict = self.frame.ix[key][column].value_counts().to_dict()
+        return_dict = (self.country_frame.ix[key]
+                       [column].value_counts().to_dict())
         return return_dict
 
-    def get_name_list(self, key=None, column=None, matchval=None):
-        match_frame = (self.frame.ix[key][self.frame.ix[key][column] ==
-                       matchval])
+    def get_city_list(self, code=None, column=None, matchval=None):
+        match_frame = (self.country_frame.ix[code][self.country_frame.ix[code]
+                       [column] == matchval])
         match_list = match_frame['name'].tolist()
         return sorted(match_list)
 
+    def get_15k_total_for_country(self, code):
+        return int(self.country_frame.ix[code]['name'].count())
+
     @property
     def country_codes(self):
-        return self.frame.index.unique().tolist()
+        return self.country_frame.index.unique().tolist()
 
 
 def build_response_dict(country_code):
     if country_code in store.country_codes:
             value_counts = store.get_counts(key=country_code,
                                             column='in target')
-            print value_counts.keys()
-            total_over_15k = int(store.frame.ix[country_code]
-                                 ['name'].count())
+            total_over_15k = store.get_15k_total_for_country(country_code)
             num_cities_covered = int(value_counts[True] if True in value_counts
                                      else 0)
             percentage_covered = str(D(100 * (value_counts[True] /
                                        float(total_over_15k))).quantize(PREC)
                                      if True in value_counts else 0)
-            cities_covered = (store.get_name_list(key=country_code,
+            cities_covered = (store.get_city_list(code=country_code,
                               column='in target', matchval=True)
                               if num_cities_covered else [])
-            cities_not_covered = (store.get_name_list(key=country_code,
+            cities_not_covered = (store.get_city_list(code=country_code,
                                   column='in target', matchval=False))
     else:
         '''
@@ -76,22 +81,22 @@ def build_response_dict(country_code):
 
     return response_dict
 
-
+# Given in geonames.org spec
 colnames = ['geonameid', 'name', 'asciiname', 'alternatenames', 'latitude',
             'longitude', 'feature class', 'feature code', 'country code',
             'cc2', 'admin1 code', 'admin2 code', 'admin3 code', 'admin4 code',
             'population', 'elevation', 'dem', 'timezone', 'modification date']
 
 app = Flask(__name__)
-store = DataStore(colnames=colnames, table_url='static/data/cities15000.txt')
+store = CountryDataStore(colnames=colnames,
+                         table_url='static/data/cities15000.txt')
 
 
 @app.route('/')
 @app.route('/index')
 def home():
-    print 'inside home'
     # return render_template('index.html')
-    return render_template('example.html')
+    return render_template('index.html')
 
 
 @app.route('/country-data', methods=['POST'])
@@ -102,7 +107,6 @@ def country():
 
 
 def main():
-
     app.run(debug=True)
 
 if __name__ == '__main__':
